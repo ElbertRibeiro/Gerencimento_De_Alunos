ea795446c9872b008da8fbad5ea26e7711d33c1f
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-11 10:56:48 -0300

    update package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Student-Management-System',
+   version='1.0',
+   description='A useful module',
+   author='Elbert Ribeiro',
+   author_email='elbertprofissional@gmail.com',
+   #packages=['foo'],  #same as name
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+)
