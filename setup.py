143f0e922ae196ac95496d09e7e7d8f22c97a745
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-11 22:13:24 -0300

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-   name='Distutils',
+   name='packages',
    version='1.0',
    description='A useful module',
    author='Elbert Ribeiro',
    author_email='elbertprofissional@gmail.com',
    package = ['distutils', 'distutils.command'],
-   package_dir = {'': 'app/packages'}
+   package_dir = {'': 'app/packages'},
+   python_requires='>=3',
+   install_requires=['sqlite3', 'wxPython'],
 )
