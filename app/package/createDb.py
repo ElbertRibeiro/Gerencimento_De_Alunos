57f26c7f107c23576cbb77b31fc6a3e8f840e042
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-12 12:03:46 -0300

    update createDB

--- app/package/createDb.py
+++ app/package/createDb.py
@@ -1,12 +1,12 @@
 import sqlite3
 
 def createDb():
-    conn = sqlite3.connect('..database.alunos.db')
+    conn = sqlite3.connect('alunos.db')
     # definindo um cursor
     cursor = conn.cursor()
     # criando uma tabela (schema)
     cursor.execute("""
-        CREATE TABLE ..database.alunos (
+        CREATE TABLE alunos (
             id_numpasta TEXT NOT NULL,
             nome TEXT NOT NULL,
             idade INTEGER,
@@ -18,9 +18,8 @@ def createDb():
             fone TEXT,
             cidade TEXT,
             responsavel TEXT NOT NULL
-            );""")
-    # desconectando...
-    # encerrando a conecção com o db
+            );
+        """)
     conn.close()
 
     return ('Tabela criada com sucesso')
