99d778f0a4b61bbf3caebe883dc7234af060fa40
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-11 22:05:21 -0300

    update project

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-   name='Student-Management-System',
+   name='Distutils',
    version='1.0',
    description='A useful module',
    author='Elbert Ribeiro',
    author_email='elbertprofissional@gmail.com',
-   #packages=['foo'],  #same as name
-   install_requires=['sqlite3', 'wxPython'], #external packages as dependencies
+   package = ['distutils', 'distutils.command'],
+   package_dir = {'': 'app/packages'}
 )
