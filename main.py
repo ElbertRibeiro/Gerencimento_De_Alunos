53095105c24356641c08b4040dadbdb00400f46b
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2021-01-20 19:05:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ import app.package.deleteDb
 import app.package.changeDb
 import app.package.searchDb
 from time import sleep
-
-
-
-
 opcao = 0
 while opcao !=7:
-    
     print("""
         1 = Criar banco de dados
         2 = Ler dados
@@ -21,34 +16,23 @@ while opcao !=7:
         6 = Pesuisar aluno
         7 = Sair do programa
         """)
-    
     opcao = int(input("Escolha sua opcao:"))
-    
     if opcao == 1:
         print( app.package.createDb.createDb() )
-
     elif opcao == 2:
         print ( app.package.consultDb.consultDb() )
-
     elif opcao == 3:
         print ( app.package.insertDb.insertDb() )
-
     elif opcao == 4:
         print( app.package.deleteDb.deleteDb() )
-    
     elif opcao == 5:
         print( app.package.changeDb.changeDb() )
-
     elif opcao == 6:
         print( app.package.searchDb.searchDb() )
-
     elif opcao == 7:
         print('Finalizando...')
-
     else:
         print('Opção invalida, tente novamente!!!')
     print('=-=' * 10)
     sleep(2)
-
-
 print("---Saindo do programa---")
