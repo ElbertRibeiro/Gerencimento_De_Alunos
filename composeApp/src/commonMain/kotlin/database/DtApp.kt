b7d77f76139821ce04d8e06f7149f02b970aa3bd
ElbertRibeiro <elbertprofissional@gmail.com>
2024-07-30 20:13:38 -0300

    feat: add sqldelight

--- composeApp/src/commonMain/kotlin/database/DtApp.kt
+++ composeApp/src/commonMain/kotlin/database/DtApp.kt
@@ -1,17 +1,38 @@
 package database
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyApp() {
     var users by remember { mutableStateOf(listOf<User>()) }
+    var name by remember { mutableStateOf("") }
+    users = UserRepository.getAllUsers()
+
 
     Column {
+        Text("Formulário de Teste", style = MaterialTheme.typography.h6)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nome") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
         Button(onClick = {
-            UserRepository.addUser("John Doe", 30)
+            UserRepository.addUser(name, 30)
             users = UserRepository.getAllUsers()
         }) {
             Text("Add User")
