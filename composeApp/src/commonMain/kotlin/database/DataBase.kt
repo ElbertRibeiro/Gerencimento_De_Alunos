0a0addccbbe123d876962d848c97d68942a9d538
ElbertRibeiro <elbertprofissional@gmail.com>
2024-07-30 20:47:26 -0300

    feat: add sqldelight

--- composeApp/src/commonMain/kotlin/database/DataBase.kt
+++ composeApp/src/commonMain/kotlin/database/DataBase.kt
@@ -1,7 +1,15 @@
 package database
 
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun initDatabase() {
     Database.connect(url = "jdbc:sqlite:data.db", driver = "org.sqlite.JDBC")
+}
+
+fun createTables() {
+    transaction {
+        SchemaUtils.create(Users)
+    }
 }
--- composeApp/src/commonMain/kotlin/database/Users.kt
+++ composeApp/src/commonMain/kotlin/database/Users.kt
@@ -1,16 +1,8 @@
 package database
 
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
 
 object Users : IntIdTable() {
     val name = varchar("name", 50)
     val age = integer("age")
-}
-
-fun createTables() {
-    transaction {
-        SchemaUtils.create(Users)
-    }
 }
