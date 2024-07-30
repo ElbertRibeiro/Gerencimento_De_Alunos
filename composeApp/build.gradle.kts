0ce558bb6070cdd570c3fd75a52abf8fef5173d3
ElbertRibeiro <elbertprofissional@gmail.com>
2024-07-30 18:01:22 -0300

    feat: adicionando dependencia do mongo

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.navigation.compose)
+            implementation(libs.mongo)
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
