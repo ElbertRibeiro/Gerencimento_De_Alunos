4a218cb4109a44ec83125f16da89643b1f975318
ElbertRibeiro <elbertprofissional@gmail.com>
2024-07-29 21:22:01 -0300

    feat: removendo pagina home

--- composeApp/src/commonMain/kotlin/view/BuscarAlunos.kt
+++ composeApp/src/commonMain/kotlin/view/BuscarAlunos.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
