3dea925acce05b702afeb076ad19f2686c0d7156
ElbertRibeiro <elbertprofissional@gmail.com>
2024-08-01 18:13:57 -0300

    feat: adicionando butão para excluir aluno

--- composeApp/src/commonMain/kotlin/component/StudantListComponent.kt
+++ composeApp/src/commonMain/kotlin/component/StudantListComponent.kt
@@ -1,16 +1,16 @@
 package component
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import domain.studant.dto.User
 
@@ -47,6 +47,18 @@ object StudantListComponent {
                             Icon(
                                 imageVector = Icons.Default.Edit,
                                 contentDescription = "Edit",
+                            )
+                        }
+
+                        Spacer(Modifier.width(8.dp))
+
+                        Button(
+                            onClick = { },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete",
                             )
                         }
                     }
